db4f2fb135ecea265aa2070e7113b147f8b8f199
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-08-31 14:32:49 +0200

    rdy

---
+++ bin/greeter.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+void main() {
+  print('Hallo !');
+  print('Gebe mir bitte deinen Vorname.');
+
+  String? vorname = stdin.readLineSync();
+  while (vorname == null || vorname.isEmpty) {
+    print('Es ist keine Eingabe erfolgt !!');
+    print('Vorname eingeben !!!');
+    vorname = stdin.readLineSync();
+  }
+  print('Vorname: $vorname');
+  print('Jetzt Nachname eingeben !');
+
+  String? nachname = stdin.readLineSync();
+  while (nachname == null || nachname.isEmpty) {
+    print('Es ist keine Eingabe erfolgt !!');
+    print('Nachname eingeben !!!');
+    nachname = stdin.readLineSync();
+  }
+  print('Vollständiger Name: $vorname $nachname.');
+  print('Alter eingeben.');
+
+  String? alter = stdin.readLineSync();
+  int? alterZahl = int.tryParse(alter ?? '');
+  while (alterZahl == null) {
+    print('Ungültige Eingabe');
+    print('Gebe dein alter ein !!!');
+    alter = stdin.readLineSync();
+    alterZahl = int.tryParse(alter ?? '');
+  }
+  print('Alter: $alterZahl');
+  print(
+    'Wähle dein Geschlecht.\n'
+    'Männlich = 1\n'
+    'Weiblich = 2\n',
+  );
+
+  String? geschlecht = stdin.readLineSync();
+  int? auswahlGeschlecht = int.tryParse(geschlecht ?? '');
+  while (auswahlGeschlecht == null) {
+    print('Es ist keine Eingabe erfolgt !!');
+    print('Geschlecht Eingeben !!!');
+    geschlecht = stdin.readLineSync();
+    auswahlGeschlecht = int.tryParse(geschlecht ?? '');
+  }
+  if (auswahlGeschlecht == 1) {
+    print('Geschlecht: Männlich');
+  } else {
+    print('Geschlecht: Weiblich');
+  }
+
+  if(alterZahl < 40){
+    print('Hallo $vorname');
+  }
+  else if(alterZahl >= 40 && auswahlGeschlecht == 1){
+    print('Guten Tag Herr $vorname $nachname');
+  }
+   else if(alterZahl >= 40 && auswahlGeschlecht == 2){
+    print('Guten Tag Frau $vorname $nachname');
+  }
+}
